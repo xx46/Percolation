5329a9ba4f635276561c5ec2eaadd05567eacd8a
Owen Astrachan <ola@cs.duke.edu>
2017-10-12 01:25:45 -0400

    initial commit

---
+++ src/PercolationStats.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+/**
+ * Compute statistics on Percolation after performing T independent experiments on an N-by-N grid.
+ * Compute 95% confidence interval for the percolation threshold, and  mean and std. deviation
+ * Compute and print timings
+ * 
+ * @author Kevin Wayne
+ * @author Jeff Forbes
+ * @author Josh Hug
+ */
+
+public class PercolationStats {
+	public static int RANDOM_SEED = 1234;
+	public static Random ourRandom = new Random(RANDOM_SEED);
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
