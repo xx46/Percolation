b632f120e694478a0f2111a6305931dc867b90cd
Ava Xu <xx46@duke.edu>
2017-10-31 03:27:43 -0400

    version 1

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,21 +1,81 @@
-import java.util.*;
+//NetID: xx46
 
-/**
- * Compute statistics on Percolation after performing T independent experiments on an N-by-N grid.
- * Compute 95% confidence interval for the percolation threshold, and  mean and std. deviation
- * Compute and print timings
- * 
- * @author Kevin Wayne
- * @author Jeff Forbes
- * @author Josh Hug
- */
+import java.util.*;
+import java.util.Random;
 
 public class PercolationStats {
 	public static int RANDOM_SEED = 1234;
 	public static Random ourRandom = new Random(RANDOM_SEED);
+	private double[] resultSet;
+	private static double start;
+	private static double end;
 	
 	
-	public static void main(String[] args) {
-		
+	// perform T independent experiments on an N-by-N grid
+	public PercolationStats(int N, int T){
+		if (N<=0||T<=0)
+			throw new IllegalArgumentException("Index is out of bounds.");
+		resultSet = new double[T];
+		ArrayList<int[]> grid = new ArrayList<>();
+		for (int h=0;h<N;h+=1) {
+			for (int i=0; i<N; i+=1) {
+				int[] coordinate = {h,i};
+				grid.add(coordinate);
+			}
+		}
+		double[] times = new double[T];
+		for (int k=0;k<T;k+=1) {
+			Collections.shuffle(grid,ourRandom);
+			start = System.nanoTime();
+			IUnionFind finder = new QuickUWPC(N);
+			IPercolate percolator = new PercolationUF(N,finder);
+			for (int f=0;f<N*N;f+=1) {
+				percolator.open(grid.get(f)[0], grid.get(f)[1]);
+				if (percolator.percolates()) {
+					resultSet[k] = percolator.numberOfOpenSites()/(Math.pow(N,2));
+					break;
+				}
+			}
+		end = System.nanoTime();
+		times[k] = (end-start)/1e9;
+		}
+	}
+	
+	
+	public double mean(){
+		double sum = 0;
+		for (int i=0; i<resultSet.length; i++){
+			sum+=resultSet[i];
+		}
+		return sum/resultSet.length;
+	}
+	
+
+	public double stddev(){
+		double dev = 0;
+		for (int i=0; i<resultSet.length; i++){
+			dev+=(resultSet[i]-this.mean())*(resultSet[i]-this.mean());
+		}
+		return Math.sqrt(dev/(resultSet.length-1));
+	}
+	
+
+	public double confidenceLow(){
+		return (this.mean()-1.96*stddev()/Math.sqrt(resultSet.length));
+	}
+	
+
+	public double confidenceHigh(){
+		return (this.mean()+1.96*stddev()/Math.sqrt(resultSet.length));
+	}
+	
+
+	public static void main(String[] args){
+		PercolationStats p = new PercolationStats(80,20);
+		System.out.println("mean: "+p.mean());
+		System.out.println("Standard Deviation: "+p.stddev());
+		System.out.println("low endpoint of 95% confidence interval: "+p.confidenceLow());
+		System.out.println("high endpoint of 95% confidence interval: "+p.confidenceHigh());
+		System.out.printf("total time = %2.3f\n", (end-start));	
 	}
 }
